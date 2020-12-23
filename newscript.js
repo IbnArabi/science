c5249ecd914c0082a99a1939d2bd9f011124e42e
goodcodingstudios <62258613+goodcodingstudios@users.noreply.github.com>
2020-12-23 17:56:06 +0300

    Create newscript.js

---
+++ newscript.js
@@ -0,0 +1,5 @@
+var em = document.getElementById("email").value
+var pass = document.getElementById("password").value
+
+if(em === "shayansaeed@islamic-institute.tk" || em === "izmas@islamic-institute.tk" || em === "abdullaz@islamic-institute.tk" || em === "omar_riaz@islamic-institute.tk" || em === "rohariaz@islamic-institute.tk" || em === "shah.m@islamic-institute.tk" || em === "nadiyas@islamic-institute.tk" || em === "yousufm@islamic-institute.tk" || em === "hamzasaeed@islamic-institute.tk" && pass === "onlygodALLAH" ||  pass === "596321g" || pass === "5756430r" || pass === "5756430r" || pass === "5756430r" || pass === "islam9870" || pass === "13589721" || pass === "nadiyascent"){ let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100`;open('/users/mains', 'test', params);}
+else{alert("Please check your sign-in info and try again");
